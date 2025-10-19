6f6dafde4fcdf515f2448f0ee9916352ad5c0a1e
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-10-19 09:53:58 +0200

    Prepare project for unit and integration tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
