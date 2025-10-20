ff13a0ed7024b4395c2f33e9f96902de5de679ec
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-10-20 23:26:42 +0200

    Move user registration to newly created AuthController

---
+++ src/main/java/com/badzianga/chirp/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.badzianga.chirp.controller;
+
+import com.badzianga.chirp.exception.UserAlreadyExistsException;
+import com.badzianga.chirp.model.User;
+import com.badzianga.chirp.request.CreateUserRequest;
+import com.badzianga.chirp.response.ApiResponse;
+import com.badzianga.chirp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/auth")
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> registerUser(@RequestBody CreateUserRequest request) {
+        try {
+            User user = userService.addUser(request);
+            return ResponseEntity.ok(new ApiResponse("success", user));
+        } catch (UserAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
--- src/main/java/com/badzianga/chirp/controller/UserController.java
+++ src/main/java/com/badzianga/chirp/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.badzianga.chirp.controller;
 
-import com.badzianga.chirp.exception.UserAlreadyExistsException;
 import com.badzianga.chirp.model.User;
-import com.badzianga.chirp.request.CreateUserRequest;
 import com.badzianga.chirp.response.ApiResponse;
 import com.badzianga.chirp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
@@ -22,16 +19,6 @@ public class UserController {
     public ResponseEntity<ApiResponse> getAllUsers() {
         List<User> users = service.getAllUsers();
         return ResponseEntity.ok(new ApiResponse("success", users));
-    }
-
-    @PostMapping("/add")
-    public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request) {
-        try {
-            User user = service.addUser(request);
-            return ResponseEntity.ok(new ApiResponse("success", user));
-        } catch (UserAlreadyExistsException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
-        }
     }
 
     @GetMapping("/find/{username}")
