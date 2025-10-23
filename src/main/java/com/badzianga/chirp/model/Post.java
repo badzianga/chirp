4da1f88134d53725f75ecdaa7f4b16155f372af3
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-10-23 17:38:23 +0200

    Add auto id generation, constructor and new field to Post class

--- src/main/java/com/badzianga/chirp/model/Post.java
+++ src/main/java/com/badzianga/chirp/model/Post.java
@@ -14,11 +14,19 @@ import java.time.LocalDateTime;
 @Setter
 public class Post {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDateTime created;
+    private String content;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User author;
+
+    public Post(String content, User author) {
+        this.created = LocalDateTime.now();
+        this.content = content;
+        this.author = author;
+    }
 }
