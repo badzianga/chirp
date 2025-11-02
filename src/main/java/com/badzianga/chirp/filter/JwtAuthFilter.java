d3429d82ecb2a284139ab89e8345f6cfdcb6fda6
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-11-02 14:16:20 +0100

    Use interface instead of class

--- src/main/java/com/badzianga/chirp/filter/JwtAuthFilter.java
+++ src/main/java/com/badzianga/chirp/filter/JwtAuthFilter.java
@@ -1,7 +1,6 @@
 package com.badzianga.chirp.filter;
 
 import com.badzianga.chirp.service.JwtService;
-import com.badzianga.chirp.service.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,7 +21,7 @@ import java.io.IOException;
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-    private final UserDetailsServiceImpl userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
